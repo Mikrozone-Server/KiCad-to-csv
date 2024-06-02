1181c2aa29d34ca0bc2d963542b9f254cd32f067
Andrej Valek <andy@skyrain.eu>
2024-06-02 14:03:47 +0200

    feat(kicad-export): add support for multiple files processing

--- kicad-export.py
+++ kicad-export.py
@@ -1,5 +1,7 @@
+import os, sys
+import csv
+import argparse
 from kiutils.symbol import SymbolLib
-import sys, csv
 
 KI_VALUES = [
     "ki_description",
@@ -38,36 +40,77 @@ CSV_HEADER = [
 
 
 if __name__ == "__main__":
-    symbol_lib = SymbolLib().from_file("tests/symbols/input.kicad_sym")
-    if symbol_lib == None:
+    # process arguments
+    parser = argparse.ArgumentParser(
+        description='KiCad library symbol exporter (CSV)', add_help=False
+    )
+    parser.add_argument(
+        '-h', '--help', action='help',
+        help='Show this help message and exit'
+    )
+    parser.add_argument('-i', '--input',
+        dest='input_dirfile', action='store',
+        help='Path to input directory or single file (.kicad_sym)', required=True
+    )
+    parser.add_argument('-o', '--output', 
+        dest='output_file', action='store',
+        help='Output filename (.csv)', required=True
+    )
+
+    args = parser.parse_args()
+
+    # check if input is file or dir
+    input_files = []
+    if os.path.exists(args.input_dirfile):
+        if os.path.isfile(args.input_dirfile):
+            input_files.append(args.input_dirfile)
+        else:
+            input_files = [
+                os.path.abspath(os.path.join(args.input_dirfile, f))
+                for f in os.listdir(args.input_dirfile) if f.endswith('.kicad_sym')
+            ]
+    else:
+        print(args.input_dirfile + " does not exists!")
         sys.exit(1)
 
+
+    print("Start processing...")
+
     # write csv file
-    with open('output.csv', 'w', newline='') as csv_file:
+    with open(args.output_file, 'w', newline='') as csv_file:
         writer = csv.writer(csv_file)
 
         # write csv header
         writer.writerow(CSV_HEADER)
 
-        # loop over all symbols
-        for s in symbol_lib.symbols:
-            # print path and name
-            row = [symbol_lib.filePath, s.entryName]
+        # loop over input file/s
+        for f in input_files:
+            try:
+                symbol_lib = SymbolLib().from_file(f)
 
-            # convert properties list to dictionary for quicker access
-            props_dict = {prop.key: prop for prop in s.properties}
+                # loop over all symbols
+                for s in symbol_lib.symbols:
+                    # print path and name
+                    row = [symbol_lib.filePath, s.entryName]
 
-            # print ki_ values
-            for k in KI_VALUES:
-                row.append(props_dict.get(k).value if k in props_dict else "")
+                    # convert properties list to dictionary for quicker access
+                    props_dict = {prop.key: prop for prop in s.properties}
 
-            # print rest of properties
-            for k, p in props_dict.items():
-                if k in ALL_PROPERTIES:
-                    row.append(p.value)
-                elif k not in KI_VALUES:
-                    row.append("")
-                    print(f'"{s.entryName}": "{k}" is missing')
+                    # print ki_ values
+                    for k in KI_VALUES:
+                        row.append(props_dict.get(k).value if k in props_dict else "")
 
-            # write row to csv
-            writer.writerow(row)
+                    # print rest of properties
+                    for k, p in props_dict.items():
+                        if k in ALL_PROPERTIES:
+                            row.append(p.value)
+                        elif k not in KI_VALUES:
+                            row.append("")
+                            print(f'"{s.entryName}": "{k}" is missing')
+
+                    # write row to csv
+                    writer.writerow(row)
+            except Exception as ex:
+                print(f'"Unable to process {f}", err: {ex}')
+
+    print("Processing done")
